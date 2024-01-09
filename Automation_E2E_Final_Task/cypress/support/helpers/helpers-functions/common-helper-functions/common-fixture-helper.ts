8e0a444b6f0cc9fd908ece5ace0ed21a50a6f6ec
Maram Naqeeb <maramnaqib1998@gmail.com>
2024-01-09 16:10:15 +0200

    common

---
+++ Automation_E2E_Final_Task/cypress/support/helpers/helpers-functions/common-helper-functions/common-fixture-helper.ts
@@ -0,0 +1,10 @@
+
+class CommonFixtureHelper {
+    static loadDataFromFixture(filePath: any, fixtureName: any) {
+      cy.fixture(filePath).as(fixtureName);
+      return cy.get(`@${fixtureName}`);
+    }
+
+  }
+  export default CommonFixtureHelper;
+  
