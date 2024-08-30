259120445105859d9b80f49c477111fb4fe05d82
Maram Naqeeb <maramnaqib1998@gmail.com>
2024-08-31 01:52:42 +0300

    genericFunctions

--- Automation_E2E_Final_Task/cypress/support/helpers/helpers-functions/common-helper-functions/generic-functions.ts
+++ Automation_E2E_Final_Task/cypress/support/helpers/helpers-functions/common-helper-functions/generic-functions.ts
@@ -1,23 +1,24 @@
 class GenericFunctions {
-  
   static randomNumber() {
-    return Math.round(Math.random()*1000);
+    return Math.round(Math.random() * 1000);
   }
- 
-  
-  static currentDate() {
 
+  static currentDate() {
     const NOW_DATE = new Date();
     let day = NOW_DATE.getDate();
-    let month = NOW_DATE.getMonth() +1;
+    let month = NOW_DATE.getMonth() + 1;
     let year = NOW_DATE.getFullYear();
     let currentDate: any = `${year}-${month}-${day}`;
     return currentDate;
-
   }
-  
-  
+  static testCurrentDate() {
+    const now = new Date();
+    const expectedDate = `${now.getFullYear()}-${String(now.getDate()).padStart(
+      2,
+      "0"
+    )}-${String(now.getMonth() + 1).padStart(2, "0")}`;
+
+    return expectedDate;
+  }
 }
 export default GenericFunctions;
-
-
